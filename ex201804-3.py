d18ab82076fe2e0b7d5821dfd07765078b554f52
QiWang <wangqi0701@163.com>
2018-04-16 20:38:07 +0800

    @property only read

---
+++ ex201804-3.py
@@ -0,0 +1,18 @@
+#!/user/bin/env python3
+#coding:utf-8
+#@property only read
+class Student(object):
+	@property
+	def birth(self):
+		return self._birth
+	@birth.setter
+	def birth(self,value):
+		self._birth = value
+	@property
+	def age(self):
+		return 2018-self._birth
+
+s = Student()
+s.birth = 1995
+print(s.birth)
+print(s.age)

12b8b96a032feb6d8aa9e5edcd27ffc7a3e1c0b8
QiWang <wangqi0701@163.com>
2018-04-17 21:43:18 +0800

    refer to someone whose id is MRZxxx

--- ex201804-5.py
+++ ex201804-5.py
@@ -1,19 +1,20 @@
 #!/user/bin/env python3
 #coding:utf-8
-class Student(object):
-	def __init__(self):
-		self.name = 'Wendy'
-	'''def __getattr__(self,attr):
-		if attr == 'score':
-			return 99'''
-	def __getattr__(self,attr):
-		if attr == 'age':
-			return lambda:25
-		else:
-			raise AttributeError('\'Student\' object has no attribute \'%s\'' % attr)
+#实现一个定制类，便于链式调用
+class chain(object):
+	"""docstring for chain"""
+	def __init__(self, path = ''):
+		self.path = path
+	#调用print()打印path
+	def __str__(self):
+		return self.path
+	#当对实例进行调用a('xxx')时将path连接并返回一个新的实例
+	def __call__(self,path):
+		return chain('%s/%s' % (self.path,path))
+	def __getattr__(self,item):
+		return chain('%s/%s' % (self.path,item))
+	__repr__ = __str__
 
-s = Student()
-print(s.name) 
-#注意函数的调用方式
-print(s.age()) 
-print(s.abc) 
+c = chain() 
+print(c.users('wendy').repos('xxx')) 
+print(c.status.user.timeline.list)

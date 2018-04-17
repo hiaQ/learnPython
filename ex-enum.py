fc460006c4bd4b8bf328d7803ef707b9a5e49edc
QiWang <wangqi0701@163.com>
2018-04-17 22:11:56 +0800

    enum test from michaelliao

---
+++ ex-enum.py
@@ -0,0 +1,19 @@
+#!/user/bin/env python3
+#coding:utf-8
+#把Student的gender属性改造为枚举类型，可以避免使用字符串
+from enum import Enum
+class Gender(Enum):
+	Male = 0
+	Female = 1
+
+class Student(object):
+	def __init__(self,name,gender):
+		self.name = name
+		self.gender = gender
+bart = Student('Bart',Gender.Male)
+if bart.gender == Gender.Male:
+	print('测试通过！')
+else:
+	print('测试失败！') 
+
+	

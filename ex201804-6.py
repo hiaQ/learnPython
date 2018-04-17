ee8feccd9452c0f3ed789f92f0eb69ac5d0cf36d
QiWang <wangqi0701@163.com>
2018-04-17 22:05:32 +0800

    enum 2

--- ex201804-6.py
+++ ex201804-6.py
@@ -1,9 +1,25 @@
 #!/user/bin/env python3
 #coding:utf-8
 
-from enum import Enum
-Month = Enum('Month',('Jan','Feb','Mar','Apr','May','Jun','Jul',
-	'Aug','Sep','Oct','Nov','Dec'))
+from enum import Enum,unique
 
-for name,member in Month.__members__.items():
-	print(name,'=>',member,',',member.value) 
+#@unique装饰器可以帮我检查保证没有重复值
+@unique
+class Weekday(Enum):
+	#把Sun的value值改为0
+	Sun = 0
+	Mon = 1
+	Tue = 2
+	Wed = 3
+	Thu = 4
+	Fri = 5
+	Sat = 6 
+
+#访问枚举类型
+day1 = Weekday.Mon
+print('Weekday.Mon = ',day1)
+print('Weekday.Tue = ',Weekday.Tue)
+print('Weekday.Tue.value = ', Weekday.Tue.value)
+print(Weekday['Tue'])
+for name,member in Weekday.__members__.items():
+	print(name,'=>',member) 

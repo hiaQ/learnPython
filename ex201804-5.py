ea123b3eca5cd739bd661d9a4a852ac94da05d88
QiWang <wangqi0701@163.com>
2018-04-17 20:51:07 +0800

    __getattr__ in Class

--- ex201804-5.py
+++ ex201804-5.py
@@ -1,24 +1,16 @@
 #!/user/bin/env python3
 #coding:utf-8
-class Fib(object):
-	def __getitem__(self,n):
-		if isinstance(n,int): #n是索引
-			a,b = 1,1
-			for x in range(n):
-				a,b = b,a+b
-			return a
-		if isinstance(n,slice): #n是切片
-			start = n.start
-			stop = n.stop
-			if start is None:
-				start = 0
-			a,b = 1,1
-			L = []
-			for x in range(stop):
-				if x >= start:
-					L.append(a)
-				a,b = b,a+b
-			return L
-f = Fib()
-print(f[0:5])
-print(f[:10]) 
+class Student(object):
+	def __init__(self):
+		self.name = 'Wendy'
+	'''def __getattr__(self,attr):
+		if attr == 'score':
+			return 99'''
+	def __getattr__(self,attr):
+		if attr == 'age':
+			return lambda:25
+
+s = Student()
+print(s.name) 
+#注意函数的调用方式
+print(s.age()) 

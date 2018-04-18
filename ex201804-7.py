af47dbbb3ae9872fd60ffd1e2f95760debb2cd7d
QiWang <wangqi0701@163.com>
2018-04-18 22:21:00 +0800

    error test --https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/00143191375461417a222c54b7e4d65b258f491c093a515000---

--- ex201804-7.py
+++ ex201804-7.py
@@ -1,17 +1,23 @@
 #!/user/bin/env python3
 #coding:utf-8
-#err_reraise
-def foo(s):
-	n = int(s)
-	if n == 0:
-		raise ValueError('invalid value: %s' %s)
-	raise 10 / n 
 
-def bar():
+from functools import reduce
+def str2num(s):
 	try:
-		foo('0')
-	except ValueError as e:
-		print('ValueError!')
-		raise
+		return int(s)
+	except Exception as e:
+		print('Exception:',e)
+		return float(s)
 
-bar()
+def calc(exp):
+	ss = exp.split('+')
+	ns = map(str2num, ss)
+	return reduce(lambda acc,x:acc+x,ns)
+
+def main():
+	r = calc('100+200+345')
+	print('100+200+345 = ',r)
+	r = calc('99+88+7.6')
+	print('99+88+7.6 = ',r)
+
+main()

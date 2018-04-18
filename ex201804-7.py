456910e0a49fbd554c415279473824d6b0e95b89
QiWang <wangqi0701@163.com>
2018-04-18 22:05:31 +0800

    raise error

--- ex201804-7.py
+++ ex201804-7.py
@@ -1,18 +1,13 @@
 #!/user/bin/env python3
 #coding:utf-8
-#logging模块
-
-import logging
+#err_raise
+class FooError(ValueError):
+	pass
 
 def foo(s):
-	return 10 / int(s)
-def bar(s):
-	return foo(s)*2
-def main():
-	try:
-		bar('0')
-	except Exception as e:
-		logging.exception(e)
+	n = int(s)
+	if n == 0:
+		raise FooError('Invalid value: %s' % s)
+	return 10/n
 
-main()
-print('END') 
+print(foo('0'))

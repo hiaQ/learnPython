2293bdda915dcbeaec7722409a2a0bc95ffa1a49
QiWang <wangqi0701@163.com>
2018-04-18 21:17:50 +0800

    try...except...except...else...finally...

--- my_try.py
+++ my_try.py
@@ -2,12 +2,14 @@
 #coding:utf-8
 try:
     print('try...')
-    r = 10 / int('a')
+    r = 10 / 2
     print('result:',r)
 except ValueError as e:
 	print('ValueError:',e)
 except ZeroDivisionError as e:
 	print('ZeroDivisionError:',e)
+else:
+	print('No ERROR!')
 finally:
 	print('finally...')
-print('END')
+print('END') 

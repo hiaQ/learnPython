2ff1c76e5eb10ca18545b99c3c9da1ed89971ea1
QiWang <wangqi0701@163.com>
2018-04-18 21:42:41 +0800

    still try...except...

--- my_try.py
+++ my_try.py
@@ -1,15 +1,17 @@
 #!/user/bin/env python3
 #coding:utf-8
-try:
-    print('try...')
-    r = 10 / 2
-    print('result:',r)
-except ValueError as e:
-	print('ValueError:',e)
-except ZeroDivisionError as e:
-	print('ZeroDivisionError:',e)
-else:
-	print('No ERROR!')
-finally:
-	print('finally...')
-print('END') 
+def foo(s):
+	return 10/int(s)
+
+def bar(s):
+	return foo(s)*2
+
+def main():
+	try:
+		bar(0)
+	except Exception as e:
+		print('Error')
+	finally:
+		print('finally...')
+
+main() 

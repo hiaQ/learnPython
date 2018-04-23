bbe90cbdea362d7d0d9eac9f0e9a548a6ead8979
QiWang <wangqi0701@163.com>
2018-04-23 21:28:48 +0800

    class类 pickling3

--- myjson.py
+++ myjson.py
@@ -12,7 +12,13 @@ class Student(object):
 def student2dict(std):
 	return {'name':std.name,'age':std.age,'score':std.score}
 
+def dict2student(std):
+	return Student(std['name'],std['age'],std['score'])
+
 s = Student('Bob',20,88)
 print(json.dumps(s, default = student2dict)) 
-#给类序列化的另一种方法__dict__
+#给类序列化的另一种方法__dict__ 
 print(json.dumps(s,default = lambda obj:obj.__dict__))
+
+json_str = '{"age":20,"score":88,"name":"Bob"}'
+print(json.loads(json_str,object_hook = dict2student)) 

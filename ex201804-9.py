ca9a6522564502ec8479d8e89f7d6b2c7eca5ebe
QiWang <wangqi0701@163.com>
2018-04-24 21:47:45 +0800

    windows下子进程--在Windows下decode(GBK)

--- ex201804-9.py
+++ ex201804-9.py
@@ -1,20 +1,15 @@
 #!/user/bin/env python3
 #coding:utf-8
-from multiprocessing import Process
-import os
+'''import subprocess
 
-#子进程要执行的代码
-def run_proc(name):
-	print('Run child process %s (%s)' % 
-		(name,os.getpid()))
+print('$ nslookup www.baidu.com')
+r = subprocess.call(['nslookup','www.baidu.com'])
+print('Exit code:',r)  '''
 
-if __name__ == '__main__':
-	print('Parent process %s.' % os.getpid())
-	p = Process(target = run_proc,args = ('test',))
-	print('Child process will start.')
-	#start()方法用于启动
-	p.start()
-	#join()方法可以等待子进程结束后再继续往下运行，通常
-	#用于进程间的同步
-	p.join()
-	print('Child process end.') 
+import subprocess
+print('$nslookup')
+p = subprocess.Popen(['nslookup'],stdin = subprocess.PIPE,
+	stdout = subprocess.PIPE,stderr = subprocess.PIPE)
+output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
+print(output.decode('GBK'))
+print('Exit code:',p.returncode) 

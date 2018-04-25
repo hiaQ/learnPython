636814b6c4a6020d395ae25ea69de0680d5d5335
QiWang <wangqi0701@163.com>
2018-04-25 21:16:27 +0800

    threading.Lock() study

--- ex201804-10.py
+++ ex201804-10.py
@@ -2,17 +2,28 @@
 #coding:utf-8
 import time,threading
 
-#新线程执行的代码
-def loop():
-	print('thread %s is running...' % threading.current_thread().name)
-	n =0
-	while n < 5:
-		n = n+1
-		print('thread %s >>> %s' % (threading.current_thread().name,n))
-		time.sleep(1)
-	print('thread %s ended.' % threading.current_thread().name)
-print('thread %s is running...' % threading.current_thread().name)
-t = threading.Thread(target = loop, name = 'LoopThread')
-t.start()
-t.join()
-print('thread %s ended.' % threading.current_thread().name) 
+balance = 0
+lock = threading.Lock()
+
+def change_it(n):
+	global balance
+	balance = balance + n
+	balance = balance - n
+
+def run_thread(n):
+	for i in range(100000):
+		#先获取锁
+		lock.acquire()
+		try:
+			change_it(n)
+		finally:
+			#然后释放锁，以免后续线程无法调用该函数
+			lock.release()
+
+t1 = threading.Thread(target = run_thread, args = (5,))
+t2 = threading.Thread(target = run_thread, args = (8,))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print(balance)
